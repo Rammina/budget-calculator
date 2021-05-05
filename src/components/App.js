e04d325bd068a396fe0395859fa99e8da459471b
Rammina <AltinaSchwarzer@gmail.com>
2021-05-05 10:08:03 +0800

    adding authentication set up on mount, adding desktop width & Height state with listeners for resize

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@
 
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
-import { Router, Route, Redirect } from "react-router-dom";
+import { Router, Route, Redirect, Switch } from "react-router-dom";
+import { Auth } from "aws-amplify";
+import { authSignIn } from "../flux/actions";
 import history from "../history";
 
 import AuthenticatedRoute from "./routeWrappers/AuthenticatedRoute";
@@ -12,34 +14,79 @@ import UnauthenticatedRoute from "./routeWrappers/UnauthenticatedRoute";
 
 // to be used later
 // import {} from "./AppContext";
-// import { loadUser } from "../flux/actions/authActions";
-// import * as constants from "../utils/constants.js";
-
-onLoadAuthSetup = async () => {
-  try {
-    console.log("authentication setup...");
-
-    const { attributes } = await Auth.currentAuthenticatedUser();
-    const userId = attributes.sub;
-    console.log(attributes);
-    console.log(attributes.sub);
-
-    this.props.authSignIn({ userId, authMethod: "cognito" });
-  } catch (e) {
-    if (e !== "No current user") {
-      console.log(e);
-    }
-  }
-
-  this.setState({ isAuthenticating: false });
-};
+import * as constants from "../utils/constants.js";
 
 export const App = (props) => {
-  //note: add chat for logged in users
+  // AWS amplify/auth + GoogleAuth
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [authSignInChecked, setAuthSignInChecked] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(
+    true
+  ); /*it's going to load authenticate initially*/
+  // screen dimension
+  const { DESKTOP_WIDTH, DESKTOP_HEIGHT } = constants;
+  const [isDesktopWidth, setIsDesktopWidth] = useState(
+    window.innerWidth >= DESKTOP_WIDTH
+  );
+  const [isDesktopHeight, setIsDesktopHeight] = useState(
+    window.innerHeight >= DESKTOP_HEIGHT
+  );
+
+  // change state when swapping between desktop width and height
+  const handleResize = () => {
+    if (window.innerWidth >= DESKTOP_WIDTH) {
+      setIsDesktopWidth(true);
+      console.log("desktop with");
+    } else {
+      setIsDesktopWidth(false);
+      console.log("not desktop with");
+    }
+    if (window.innerHeight >= DESKTOP_HEIGHT) {
+      setIsDesktopHeight(true);
+      console.log("desktop height");
+    } else {
+      setIsDesktopHeight(false);
+      console.log("not desktop height");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    // cleanup function
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const onLoadAuthSetup = async () => {
+    try {
+      const { attributes } = await Auth.currentAuthenticatedUser();
+      const userId = attributes.sub;
+      props.authSignIn({ userId, authMethod: "cognito" });
+    } catch (e) {
+      if (e !== "No current user") {
+        console.log(e);
+      }
+    }
+    setIsAuthenticating(false);
+  };
+
+  useEffect(() => {
+    // always try to sign in the user to Cognito when rendering the application the first time
+    onLoadAuthSetup();
+    /*return () => {}*/
+  }, []);
+
   return <div id="app-outer-container" data-test="component-app"></div>;
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { authSignIn })(App);
+
 /*
 const mapStateToProps = (state) => ({
   isLoadingUser: state.auth.isLoading,
@@ -48,46 +95,6 @@ const mapStateToProps = (state) => ({
 });
 */
 // export default connect(mapStateToProps, { loadUser })(App);
-
-// eventually put this at the top of the component
-/*
-const { DESKTOP_WIDTH, DESKTOP_HEIGHT } = constants;
-const [isDesktopWidth, setIsDesktopWidth] = useState(
-  window.innerWidth >= DESKTOP_WIDTH
-);
-const [isDesktopHeight, setIsDesktopHeight] = useState(
-  window.innerHeight >= DESKTOP_HEIGHT
-);
-
-const handleResize = () => {
-  if (window.innerWidth >= DESKTOP_WIDTH) {
-    setIsDesktopWidth(true);
-    console.log("desktop with");
-  } else {
-    setIsDesktopWidth(false);
-    console.log("not desktop with");
-  }
-  if (window.innerHeight >= DESKTOP_HEIGHT) {
-    setIsDesktopHeight(true);
-    console.log("desktop height");
-  } else {
-    setIsDesktopHeight(false);
-    console.log("no desktop height");
-  }
-};
-
-
-
-useEffect(() => {
-  props.loadUser();
-  window.addEventListener("resize", handleResize);
-  handleResize();
-  // cleanup function
-  return () => {
-    window.removeEventListener("resize", handleResize);
-  };
-}, []);
-*/
 
 // router
 /*
